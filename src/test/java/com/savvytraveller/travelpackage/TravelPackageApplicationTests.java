2b20b45f423335f8731a22c7f925525fe8987de9
Abhilash Paul <paul.abhilash@hotmail.com>
2021-09-28 01:50:32 -0600

    initial commit

---
+++ src/test/java/com/savvytraveller/travelpackage/TravelPackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savvytraveller.travelpackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPackageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
